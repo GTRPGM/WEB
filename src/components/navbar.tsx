46c3689d5291b3d784703dcc792e8bd83cf6d7a9
jungminseo7812 <jungminseo7812@gmail.com>
2026-01-22 16:42:26 +0900

    feat: 사이드바 아이콘과 이름 사이 거리 조절

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
   }
 
     return (
-        <div className="navbar w-full border-b border-gray-200 px-4 bg-white">
+        <div className="navbar w-full border-b border-gray-200 px-4 bg-white gap-2">
           <div className="flex-none lg:hidden">
             <label htmlFor="my-drawer" className="btn btn-square btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
