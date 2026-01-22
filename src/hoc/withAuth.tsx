58c9d60e454c8f3fe7d91419d81cb5da4cc82dbb
jungminseo7812 <jungminseo7812@gmail.com>
2026-01-22 16:26:14 +0900

    feat: api 연결 코드 보완

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -3,7 +3,7 @@ import { useAuthStore } from '../store/useAuthStore';
 
 const withAuth = (Component: React.ComponentType) => {
     return (props: any) => {
-        const access_token = useAuthStore((state) => state.access_token);
+        const access_token = useAuthStore((state) => state.access_token) || JSON.parse(sessionStorage.getItem('auth-storage') || '{}')?.state?.access_token;
 
         if (!access_token) {return <Navigate to="/login" replace />;}
 
